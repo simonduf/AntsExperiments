1d4b9e023711931e5a4a6efe498a01160e1e3330
Pierre-Luc Godin <plgodin@Delta.local>
2020-04-24 12:10:43 -0400

    Player 3 joins the game

---
+++ Bot_pl/Bot_pl_Tests/Vector2iTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ants;
+using System;
+
+namespace Bot1_Tests
+{
+    [TestClass]
+    public class Vector2iTests
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            Vector2i a = new Vector2i(1, 2);
+            Vector2i b = new Vector2i(2, 3);
+            Vector2i result = a + b;
+            Assert.AreEqual(3, result.x);
+            Assert.AreEqual(5, result.y);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Vector2i a = new Vector2i(1, 2);
+            Vector2i b = new Vector2i(2, 4);
+            Vector2i result = a - b;
+            Assert.AreEqual(-1, result.x);
+            Assert.AreEqual(-2, result.y);
+        }
+
+        [TestMethod]
+        public void TestCenter1()
+        {
+            Vector2i a = new Vector2i(0, 0);
+            Vector2i b = new Vector2i(5, 7);
+            Vector2i result = a.Centered(b);
+            Assert.AreEqual(0, result.x);
+            Assert.AreEqual(0, result.y);
+        }
+
+        [TestMethod]
+        public void TestCenter2()
+        {
+            Vector2i a = new Vector2i(-1, -2);
+            Vector2i b = new Vector2i(5, 7);
+            Vector2i result = a.Centered(b);
+            Assert.AreEqual(-1, result.x);
+            Assert.AreEqual(-2, result.y);
+        }
+
+        [TestMethod]
+        public void TestCenter3()
+        {
+            Vector2i a = new Vector2i(4, 6);
+            Vector2i b = new Vector2i(5, 7);
+            Vector2i result = a.Centered(b);
+            Assert.AreEqual(-1, result.x);
+            Assert.AreEqual(-1, result.y);
+        }
+
+    }
+}
